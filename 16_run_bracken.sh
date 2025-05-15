199ecfb8a0a08eac9d8c15e7d28357af2bd900ef
jazminvaleriano <jazmin.vsaenz@gmail.com>
2025-05-15 14:21:31 +0200

    Code for running bracken

---
+++ 16_run_bracken.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=bracken_all
+#SBATCH --output=logs/bracken_all%A.out
+#SBATCH --error=logs/bracken_all%A.err
+#SBATCH --partition=epyc2
+#SBATCH --qos=job_cpu
+#SBATCH --time=1-00:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jazmin.valerianosaenz@students.unibe.ch
+
+module load Anaconda3
+eval "$(conda shell.bash hook)"
+conda activate bracken_env
+
+# Set paths
+REPORT_DIR="metagenomics_project/results/kraken_vs_standard"
+OUT_DIR="metagenomics_project/results/bracken_vs_standard"
+DB="databases/kraken2_std"
+READ_LEN=800
+LEVEL="G"
+
+# Create output directory if not exists
+mkdir -p "$OUT_DIR"
+
+# Loop through all report files
+for REPORT in "$REPORT_DIR"/*.report.txt; do
+    SAMPLE=$(basename "$REPORT" .report.txt)
+    echo "Running Bracken on $SAMPLE"
+    bracken -d "$DB" \
+            -i "$REPORT" \
+            -o "$OUT_DIR/${SAMPLE}_bracken_${LEVEL}.txt" \
+            -r "$READ_LEN" \
+            -l "$LEVEL"
+done

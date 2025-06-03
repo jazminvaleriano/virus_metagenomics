4082750b3910b756a8f64ea4a80cd22cd53b077d
jazminvaleriano <jazmin.vsaenz@gmail.com>
2025-06-03 11:54:17 +0200

    changed options in dorado

--- 00_basecalling.sh
+++ 00_basecalling.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=dorado_gpu
-#SBATCH --partition=gpu               
-#SBATCH --qos=job_gpu                 
-#SBATCH --gres=gpu:1                 
-#SBATCH --cpus-per-task=4             
+#SBATCH --partition=gpu
+#SBATCH --qos=job_gpu
+#SBATCH --gpus=rtx3090:1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=08:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=logs/dorado_gpu_%A.out
 #SBATCH --error=logs/dorado_gpu_%A.err
 #SBATCH --mail-type=END,FAIL
@@ -19,15 +19,14 @@ export PATH=/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/dorado-1.0.0
 # Paths
 POD5_DIR="/storage/research/vetsuisse_ivi/jvaleriano/20250324_1831_MN34349_FAY63474_1833a9d4/pod5"
 OUT_DIR="/storage/research/vetsuisse_ivi/jvaleriano/20250324_1831_MN34349_FAY63474_1833a9d4/Rerun_basecall_20250603"
-MODEL_PATH="/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0/dna_r10.4.1_e8.2_400bps_hac@v5.2.0.jsn"
+MODEL_DIR="/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0"
 
 mkdir -p "$OUT_DIR"
 
 # Execute Dorado basecaller
 dorado basecaller \
-  --model "$MODEL_PATH" \
-  --flowcell FLO-MIN114 \
-  --kit SQK-NBD114-96 \
-  --detect-barcode \
+  --kit-name SQK-NBD114-96 \
   --min-qscore 9 \
-  "$POD5_DIR" "$OUT_DIR"
+  --emit-fastq \
+  --output-dir "$OUT_DIR" \
+  "$MODEL_DIR" "$POD5_DIR"

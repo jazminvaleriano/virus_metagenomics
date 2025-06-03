d792fb9c9c91f193b0240c88b44134cda8e6725d
jazminvaleriano <jazmin.vsaenz@gmail.com>
2025-06-03 14:57:51 +0200

    demultiplexing after basecalling

--- 00_basecalling.sh
+++ 00_basecalling.sh
@@ -12,6 +12,7 @@
 #SBATCH --mail-user=jazmin.valerianosaenz@students.unibe.ch
 
 module load CUDA/12.6.0 
+module load SAMtools/1.13-GCC-10.3.0
 
 # Adding Dorado to PATH
 export PATH=/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/dorado-1.0.0-linux-x64/bin:$PATH
@@ -20,13 +21,26 @@ export PATH=/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/dorado-1.0.0
 POD5_DIR="/storage/research/vetsuisse_ivi/jvaleriano/20250324_1831_MN34349_FAY63474_1833a9d4/pod5"
 OUT_DIR="/storage/research/vetsuisse_ivi/jvaleriano/20250324_1831_MN34349_FAY63474_1833a9d4/Rerun_basecall_20250603"
 MODEL_DIR="/storage/research/vetsuisse_ivi/jvaleriano/tools/dorado/models/dna_r10.4.1_e8.2_400bps_hac@v5.2.0"
+DEMUX_DIR="${OUT_DIR}/demuxed"
+FASTQ_DIR="${OUT_DIR}/fastq"
 
 mkdir -p "$OUT_DIR"
+mkdir -p "$OUT_DIR/fastq"
 
-# Execute Dorado basecaller
+
+# Step 1: Execute Dorado basecaller (bam output)
 dorado basecaller \
   --kit-name SQK-NBD114-96 \
   --min-qscore 9 \
-  --emit-fastq \
   --output-dir "$OUT_DIR" \
   "$MODEL_DIR" "$POD5_DIR"
+
+# Step 2: Demultiplex BAM into per-barcode BAMs
+BAM_FILE=$(find "$OUT_DIR" -name "*.bam" | head -n 1)
+dorado demux --no-classify --output-dir "$DEMUX_DIR" "$BAM_FILE"
+
+# Step 3: Convert each BAM to FASTQ using samtools
+for BAM in "$DEMUX_DIR"/*.bam; do
+  BARCODE=$(basename "$BAM" .bam)
+  samtools fastq "$BAM" > "$FASTQ_DIR/${BARCODE}.fastq"
+done

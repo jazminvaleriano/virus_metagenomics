527b4c6dbb621ef3da5c10f540f4f30b6da04f38
jazminvaleriano <jazmin.vsaenz@gmail.com>
2025-06-11 15:21:36 +0200

    rerunning minimap

--- 02_filter_host_minimap2.sh
+++ 02_filter_host_minimap2.sh
@@ -13,20 +13,20 @@ module load SAMtools/1.13-GCC-10.3.0
 
 # Paths
 GENOME="references/pig/pig.mmi"
-READS_DIR="raw_data"
-OUT_DIR="filtered_reads"
+READS_DIR="/storage/research/vetsuisse_ivi/jvaleriano/20250324_1831_MN34349_FAY63474_1833a9d4/Rerun_basecall_20250603/fastq"
+OUT_DIR="filtered_reads/second_basecall"
 
 mkdir -p "$OUT_DIR"
 
-for fq in "$READS_DIR"/*.fastq.gz; do
-    sample=$(basename "$fq" .fastq.gz)
+for fq in "$READS_DIR"/*.fastq; do
+    sample=$(basename "$fq" .fastq)
     
     # Map reads to pig genome
     minimap2 -t 4 -ax map-ont "$GENOME" "$fq" | \
     samtools view -@ 4 -b -f 4 -o "$OUT_DIR/${sample}_unmapped.bam" -
 
     # Convert unmapped BAM to FASTQ
-    samtools fastq -@ 4 "$OUT_DIR/${sample}_unmapped.bam" | gzip > "$OUT_DIR/${sample}_filtered.fastq.gz"
+    samtools fastq -@ 4 "$OUT_DIR/${sample}_unmapped.bam" | gzip > "$OUT_DIR/${sample}_filtered.fastq"
 
     # Clean up BAM
     rm "$OUT_DIR/${sample}_unmapped.bam"
